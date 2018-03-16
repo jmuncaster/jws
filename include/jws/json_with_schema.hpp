c77f488646e53986869e846f1d8abbc89d8bdbea
Justin Muncaster <justin@muncaster.io>
2018-03-16 11:46:00 -0700

    Copy code for a local file loader

--- include/jws/json_with_schema.hpp
+++ include/jws/json_with_schema.hpp
@@ -7,6 +7,7 @@
 
 namespace jws {
   using nlohmann::json;
+  using nlohmann::json_uri;
   using nlohmann::json_schema_draft4::json_validator;
 
   inline json load_json(const std::string& filename) {
@@ -19,8 +20,21 @@ namespace jws {
     return document;
   }
 
+  inline void loader(const json_uri& uri, json& schema) {
+    std::fstream s(uri.path().c_str());
+    if (!s.good())
+      throw std::invalid_argument("could not open " + uri.url() + " for schema loading\n");
+
+    try {
+      s >> schema;
+    }
+    catch (std::exception &e) {
+      throw e;
+    }
+  }
+
   inline json_validator load_validator(const json& schema) {
-    json_validator validator;
+    json_validator validator(loader);
     validator.set_root_schema(schema);
     return validator;
   }
